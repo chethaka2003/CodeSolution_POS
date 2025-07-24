6823be576cafa7b86258741cf3f4f398f8c14a5d
Chethaka <071c.krishanthi@gmail.com>
2025-07-25 01:22:21 +0530

    Commited version 0.1

--- src/main/java/DataBase/CustomerTableDB.java
+++ src/main/java/DataBase/CustomerTableDB.java
@@ -1,4 +1,11 @@
 package DataBase;
 
+import java.sql.Connection;
+
 public class CustomerTableDB {
+
+    //Connecting with database
+    private static final Connection connection = ConnectDB.getConnection();
+    private static final String tableName = "customer";
+
 }
